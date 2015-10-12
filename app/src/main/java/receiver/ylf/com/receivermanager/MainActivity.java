c429d4225f6c17b572979e44da7cddf60f928529
yanglangfei <185601452@qq.com>
2015-10-12 17:28:05 +0800

    自定义折线图、柱状图

--- app/src/main/java/receiver/ylf/com/receivermanager/MainActivity.java
+++ app/src/main/java/receiver/ylf/com/receivermanager/MainActivity.java
@@ -1,11 +1,20 @@
 package receiver.ylf.com.receivermanager;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ViewFlipper;
+
+import java.util.HashMap;
+
+import receiver.ylf.com.receivermanager.com.ylf.com.ylf.view.HistogramView;
+import receiver.ylf.com.receivermanager.com.ylf.model.HistogramData;
+/*
+     ViewFlipper 类的使用    主要用于APP导航界面静态页面
+ */
 
 public class MainActivity extends Activity {
     private ViewFlipper viewFlipper;
@@ -17,7 +26,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initWidget();
+       initWidget();
 
     }
 
@@ -27,6 +36,7 @@ public class MainActivity extends Activity {
         View view1=inflater.inflate(R.layout.pager1,null);
         View view2=inflater.inflate(R.layout.pager2,null);
         View view3=inflater.inflate(R.layout.pager3,null);
+        //ViewFlipper 添加View
         viewFlipper.addView(view1);
         viewFlipper.addView(view2);
         viewFlipper.addView(view3);
@@ -40,7 +50,9 @@ public class MainActivity extends Activity {
                 break;
             case  MotionEvent.ACTION_UP:
                 end=event.getX();
+                //设置滑动监听
                 if(end>start){
+                    //设置滑动的动画
                     viewFlipper.setInAnimation(this,R.anim.in_leftright);
                     viewFlipper.setOutAnimation(this,R.anim.out_rightleft);
                     viewFlipper.showNext();
@@ -56,3 +68,4 @@ public class MainActivity extends Activity {
         return super.onTouchEvent(event);
     }
 }
+
